b1fab543ac35b2350e32307f23b40f06cb6c25fa
Voloshin_Sergey <4voloshin@gmail.com>
2021-06-04 22:48:48 +0300

    feat: add function for addition new book

--- script.js
+++ script.js
@@ -3,41 +3,44 @@ const data = [
     cover: 'assets/img/js_jquery.jpg',
     title: 'JavaScript и jQuery',
     author: 'Макфарланд Дэвид',
-    year: '2016'
+    year: '2016',
   },
   {
     cover: 'https://img-gorod.ru/24/582/2458293_detail.jpg',
     title: 'Изучаем программирование на JavaScript',
     author: 'Эрик Фримен, Элизабет Робсон',
-    year: '2017'
+    year: '2017',
   },
   {
     cover: 'https://cdn1.ozone.ru/multimedia/c1200/1007123068.jpg',
     title: 'Секреты JavaScript ниндзя',
     author: 'Бэар Бибо, Джон Резиг',
-    year: '2017'
+    year: '2017',
   },
   {
     cover: 'https://cdn1.ozone.ru/multimedia/c1200/1015269939.jpg',
     title: 'ES6 и не только',
     author: 'Кайл Симпсон',
-    year: '2017'
+    year: '2017',
   },
-]
+];
 const app = document.querySelector('#books-container');
-const addBtn = document.querySelector('.button_add')
+const addBtn = document.querySelector('.button_add');
 
 // render function
-const render = (data) => {
-const bookList = document.createElement('ul')
-bookList.classList.add('books-list')
-app.appendChild(bookList)
-data.forEach(item => {
-  bookList.insertAdjacentHTML('beforeend',
-    `
+const render = () => {
+  app.innerHTML = '';
+
+  const bookList = document.createElement('ul');
+  bookList.classList.add('books-list');
+  app.appendChild(bookList);
+  data.forEach((item) => {
+    bookList.insertAdjacentHTML(
+      'beforeend',
+      `
     <li class="item">
     <div class="item__cover">
-      <img src="${item.cover}" alt=${item.title} />
+      <img src="${item.cover}" alt=${item.title} width="150" />
     </div>
     <div class="item__description">
       <p class="description__title">${item.title}</p>
@@ -49,22 +52,38 @@ data.forEach(item => {
       <button class="button button_remove">Удалить</button>
     </div>
   </li>
-    `
-  )
-});
-}
+    `,
+    );
+  });
+};
 
-// add book function
-const addBook = (evt) => {
-  let title = 'Редактирование книги'
+// add new book function
+const addBook = () => {
+  const newBook = {};
+  const titleBook = document.getElementById('title').value;
+  const authorBook = document.getElementById('author').value;
+  const yearBook = document.getElementById('year').value;
+  const coverBook = document.getElementById('cover').value;
 
-  if(evt.target === addBtn) {
-    title = 'Добавить книгу'
+  newBook.cover = coverBook;
+  newBook.title = titleBook;
+  newBook.author = authorBook;
+  newBook.year = yearBook;
+
+  data.push(newBook);
+  render();
+};
+
+// render form for book card
+const formForBook = (evt) => {
+  let title = 'Редактирование книги';
+
+  if (evt.target === addBtn) {
+    title = 'Добавить книгу';
   }
 
   app.innerHTML = '';
-  app.innerHTML =
-  `
+  app.innerHTML = `
   <form class="book-form">
   <h3 class="book-form__title">${title}</h3>
   <div class="book-form__group">
@@ -77,18 +96,24 @@ const addBook = (evt) => {
   </div>
   <div class="book-form__group">
     <label for="year">Год издания</label>
-    <input class="book-form__input" type="number" id="year" placeholder="Введите год" max="2017" required />
+    <input class="book-form__input" type="number" id="year" placeholder="Введите год" max="2017" min="2000" required />
   </div>
   <div class="book-form__group">
     <label for="cover">Изображение</label>
     <input class="book-form__input" type="text" id="cover" placeholder="Добавьте обложку книги" required />
   </div>
-    <button class="button button_save" type="submit">Сохранить</button>
+    <button class="button button_save" type="button">Сохранить</button>
     <button class="button button_cancel" type="button">Отменить</button>
 </form>
-  `
-}
+  `;
 
-addBtn.addEventListener('click', addBook)
+  const cancelBtn = app.querySelector('.button_cancel');
+  cancelBtn.addEventListener('click', render);
 
-render(data)
+  const saveBtn = app.querySelector('.button_save');
+  saveBtn.addEventListener('click', addBook);
+};
+
+addBtn.addEventListener('click', formForBook);
+
+render();
